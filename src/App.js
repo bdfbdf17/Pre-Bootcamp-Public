b12b81edf6cd5e114e4f08548dc167a37f3d4123
bdfbdf17 <bdfbdf17@gmail.com>
2023-06-11 22:38:04 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AnimatedText from './Components/AnimatedText';
+import FancyButton from './Components/FancyButton ';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="moving-border top"></div>
+      <div className="moving-border right"></div>
+      <div className="moving-border bottom"></div>
+      <div className="moving-border left"></div>
+      <AnimatedText />
+      <FancyButton />
     </div>
   );
 }
